732713527d436a4f8bf7475e5e45c1f0230982f3
DMaitia <dariusmaitia@gmail.com>
2016-09-25 22:53:23 -0300

    Commiteo 1

---
+++ TPOrga.c
@@ -0,0 +1,95 @@
+/*
+ ============================================================================
+
+ Name        : TPOrga.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct matriz {
+  int ancho;
+  int largo;
+  char *matriz;
+} t_matriz;
+
+void matrizConstructor(t_matriz *matriz, int ancho, int largo) {
+  matriz->ancho = ancho;
+  matriz->largo = largo;
+  matriz->matriz = malloc(sizeof(char) * (ancho * largo));
+  memset(matriz->matriz, 0, ancho * largo);
+}
+
+void matrizDestructor(t_matriz *matriz) {
+  free(matriz->matriz);
+  matriz->matriz = NULL;
+}
+
+void setCelda(t_matriz *matriz, int fila, int columna, bool valor) {
+  matriz->matriz[columna + fila * matriz->ancho] = valor;
+}
+
+bool getCelda(t_matriz *matriz, int fila, int columna) {
+  return matriz->matriz[columna + fila * matriz->ancho];
+}
+
+// datosValidos
+// para validar que la fila y columna pasadas por el archivo no queden fuera de
+// rango
+bool datosValidos(t_matriz *matriz, int fila, int columna) {
+  if ((fila > matriz->largo - 1) || (columna > matriz->ancho - 1))
+    return false;
+  else
+    return true;
+}
+
+// cargarMatriz
+// carga la matriz a partir del archivo
+void cargarMatriz(t_matriz *matriz, string archivo) {
+  bool fin;
+
+  FILE *fp = fopen(archivo, "rb");
+  fin = false;
+  while (fscanf(fp, "%d %d", fila, columna) != 0 &&
+         !fin) {  // No estoy seguro si lee bien, chekear
+
+    if (!datosValidos(matriz, fila, columna)) {
+      fputs(stderr, "Error de coordenadas.");
+      fin = true;
+    } else {
+      setCelda(matriz, fila, columna, true);
+    }
+  }
+  fclose(fp);
+}
+
+void juego(int *argv) {
+
+  t_matriz matriz;
+  int iteraciones;
+  string archivo;
+
+  iteraciones = argv[1];
+  archivo = argv[4];
+
+  matrizConstructor(&matriz, argv[2], argv[3]);
+
+  cargarMatriz(&matriz, archivo);
+  for (int i = 0; i < iteraciones; i++) {
+    // vecinos
+  }
+
+  matrizDestructor(&matriz);
+}
+
+int main(void) {
+  puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+  return EXIT_SUCCESS;
+}
