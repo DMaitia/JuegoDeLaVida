66946a50bc588d0c8b7ade2b0b3d4d9c4784ba25
kurepa <cristian3629@gmail.com>
2016-09-29 00:55:11 -0300

    subiendo algo de vecinos.s

--- vecinos.s
+++ vecinos.s
@@ -14,14 +14,30 @@ main:
 	sw	$gp,8($sp)
 	move	$fp,$sp
 	//inicializo las variables
-	sw $0,finH($sp)
-	sw $0,finV($sp)
-	sw $0,inicioH($sp)
-	sw $0,inicioV($sp)
+	sw $zero,finH($sp)
+	sw $zero,finV($sp)
+	sw $zero,inicioV($sp)
 
-	//inicioV = i - 1;
 	sw $a0,32($sp) //guardo a
 	sw $a1,36($sp) //guardo i
 	sw $a2,40($sp) //guardo j
 	sw $a3,44($sp) //guardo M
-	lw $t0,$a1,1
+
+	//inicioV = i - 1; no si se puede hacer directo con el -1
+	add $t0,$a1,-1
+	sw $t0,inicioV($sp)
+	//if (inicioV < 0) inicioV = 0;
+	blez $t0,inicioVCero
+	//inicioH = j - 1;
+	add $t0,$a2,-1
+	sw $t0,inicioH($sp)
+	blez $t0,inicioHCero
+	//finV = i + 1;
+	add $t0,$a1,1
+	sw $t0,finV($sp)
+	//if (finV > N - 1)
+	//cargo N-1 en t1
+	add $t1,$a4,-1
+	
+inicioVCero:	sw $zero,inicioV($sp)
+inicioHCero:	sw $zero,inicioH($sp)
